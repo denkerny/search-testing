22a5d8a19125a1b6e2282ea70a167dcfa6f9a0fa
kernyden <kernyden@gmail.com>
2018-05-31 13:44:50 +0300

    Update clients-list.component.ts

--- src/app/clients/clients-list/clients-list.component.ts
+++ src/app/clients/clients-list/clients-list.component.ts
@@ -1,39 +1,36 @@
-import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ClientsService } from '../clients.service';
 import { Observable, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators'
+
 
 @Component({
   selector: 'app-clients-list',
   templateUrl: './clients-list.component.html',
-  styleUrls: ['./clients-list.component.css']
+  styleUrls: [ './clients-list.component.css' ]
 })
-export class ClientsListComponent implements OnInit, OnDestroy {
-  @Output() showDetails = new EventEmitter<object>()
-
-  clients: object[]
+export class ClientsListComponent implements OnInit {
+  @Output() details = new EventEmitter<object>()
   subscription: Subscription
-  sortedClients: object[]
+  clients: object[]
+  newClients: object[]
   selectedIndex: number = null;
 
-  constructor(private clientsService: ClientsService) { }
+  constructor(private clientsService: ClientsService){}
 
-  ngOnInit() {
+  ngOnInit(): void{
     this.getClientsList();
   }
-
   getClientsList(): void{
-    this.subscription = this.clientsService.getList().subscribe((clients) => this.clients = clients);
+    this.subscription = this.clientsService.getList()
+        .subscribe((clients) => this.clients = clients )
   }
 
-  onClientTrClick(client): void{
-    this.showDetails.emit(client)
+  onClientClick(client): void{
+    this.details.emit(client)
   }
 
-  ngOnDestroy(): void{
-    this.subscription.unsubscribe();
-  }
-
-  getSortedClients(){
+  get(){
     let arr = [];
     this.clients.map(function (el){
       let obj = {};
@@ -44,8 +41,9 @@ export class ClientsListComponent implements OnInit, OnDestroy {
     })
     return this.newClients = arr;
   }
-  
-  setIndex(index: number) {
-    this.selectedIndex = index;
-  }
+
+   setIndex(index: number) {
+      this.selectedIndex = index;
+   }
 }
+
